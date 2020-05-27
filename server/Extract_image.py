2af78a84eabd964a1f27b0fe1497d5e0fe1a4807
Miguel0697 <65865607+Miguel0697@users.noreply.github.com>
2020-05-27 19:12:53 +0200

    Add files via upload

---
+++ server/Extract_image.py
@@ -0,0 +1,20 @@
+def read_single_disk(image_id):
+    """ Stores a single image to disk.
+        Parameters:
+        ---------------
+        image_id    integer unique ID for image
+
+        Returns:
+        ----------
+        image       image array, (32, 32, 3) to be stored
+        label       associated meta data, int label
+    """
+    image = np.array(Image.open(disk_dir / f"{image_id}.png"))
+
+    with open(disk_dir / f"{image_id}.csv", "r") as csvfile:
+        reader = csv.reader(
+            csvfile, delimiter=" ", quotechar="|", quoting=csv.QUOTE_MINIMAL
+        )
+        label = int(next(reader)[0])
+
+    return image, label
